ad14cf67f56b716b338ee1cd904d07f7534711ac
wwp <1304719637@qq.com>
2020-08-03 16:54:33 +0800

    ver 02

--- src/main/java/cn/qphone/spring/Chinese.java
+++ src/main/java/cn/qphone/spring/Chinese.java
@@ -2,6 +2,6 @@ package cn.qphone.spring;
 
 public class Chinese extends Person {
     public void showinfo() {
-        System.out.println("听妈妈的话！！！");
+        System.out.println("听妈妈的话！！！!");
     }
 }
